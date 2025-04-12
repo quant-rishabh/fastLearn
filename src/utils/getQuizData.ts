8f8b9601c134f10413d1485e8832e67ce8d522d1
Quant_rishabh <quant.rishabh@gmail.com>
2025-04-12 19:30:52 +0800

    fix: added SubjectMeta interface to remove 'any'

--- src/utils/getQuizData.ts
+++ src/utils/getQuizData.ts
@@ -1,6 +1,12 @@
+interface SubjectMeta {
+    label: string;
+    value: string;
+    file: string;
+  }
+
 // src/utils/getQuizData.ts
 export async function getQuizData(subjectValue: string) {
-    const subjects = await fetch('/data/subjects.json').then(res => res.json());
+    const subjects: SubjectMeta[] = await fetch('/data/subjects.json').then(res => res.json());
     const subject = subjects.find((s: any) => s.value === subjectValue);
     if (!subject) throw new Error('Subject not found');
     return await fetch(`/data/${subject.file}`).then(res => res.json());
